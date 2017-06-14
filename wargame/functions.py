ae6bc4e5d8a992c79a11dfb09629974ca521761b
helewonder <wxd465@hotmail.com>
2017-06-14 20:50:45 +0800

    Default

--- wargame/functions.py
+++ wargame/functions.py
@@ -17,3 +17,15 @@ def show_health(rider, bold=False, end=' '):
         print_bold(info, end=end)
     else:
         print(info, end=end)
+
+
+def weighted_random_selection(obj1, obj2):
+    weighted_list = [id(obj1)] * 3 + [id(obj2)] * 6 + [None] * 1
+    selection = random.choice(weighted_list)
+
+    if selection == id(obj1):
+        return obj1
+    elif selection == id(obj2):
+        return obj2
+    else:
+        return None
