fc013a6da32575cb324594bebdc8bf442ab534a7
helewonder <wxd465@hotmail.com>
2017-06-14 20:51:20 +0800

    Documentation

--- wargame/hut.py
+++ wargame/hut.py
@@ -2,6 +2,23 @@ from functions import print_bold
 
 
 class Hut():
+    """Class to create hut object in the game Attack of the Orcs.
+
+    :arg int number: Hut number to be assigned.
+    :arg `AbstractRider` occupant: The new occupant of the Hut.
+
+    :ivar int number: A number assigned to this hut.
+    :ivar boolean is_acquired: A boolean flag to indicate if the hut is
+    acquired. In the current implementation this is viewed from the players
+    perspective.
+    :ivar AbstractRider occupant: The occupant of this hut. Needs to be an
+    instance of a subclass of `AbstractRider`.
+
+    .. seealso:: Where it is used --
+        :py:meth: `game.OrGame.setup_game_scenario()`
+
+    """
+
     def __init__(self, number, occupant):
         self.occupant = occupant
         self.number = number
@@ -11,7 +28,7 @@ class Hut():
         self.occupant = new_occupant
         self.is_acquired = True
         print_bold("Good Job! Hut %d acquired" % self.number)
-    
+
     @property
     def get_occupant_type(self):
         if self.is_acquired:
